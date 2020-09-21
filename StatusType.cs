af9708b32ac38ee9f98bfef1a685526b8440019d
MHAbirKhan01753523481 <71643559+MHAbirKhan01753523481@users.noreply.github.com>
2020-09-21 23:59:29 +0600

    Add files via upload

---
+++ StatusType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectMaking.Enum
+{
+    public enum StatusType
+    {
+        Lost=102,
+        Folloup=103,
+        Converted=104,
+        NewLead =105,
+        LeadManager =106,
+    }
+}
