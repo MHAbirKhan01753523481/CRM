e983f2d10bde7380102ad5648c75a49518543d06
MHAbirKhan01753523481 <71643559+MHAbirKhan01753523481@users.noreply.github.com>
2020-09-22 00:00:27 +0600

    Add files via upload

---
+++ DataTable.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectMaking.Data;
+using ProjectMaking.Models.EntityModel;
+using ProjectMaking.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjectMaking.Helper
+{
+    public class DataTable
+    {
+        private readonly AppDbContext _context;
+        //private readonly IImagePath _imagePath;
+
+        public DataTable(AppDbContext context)
+        {
+            _context = context;
+           
+        }
+
+        //public DataTable(AppDbContext context)
+        //{
+        //    _context = context;
+        //}
+         public  List<vmClient> GetVmClients()
+        {
+            List<Client> listOfClient = _context.Clients.ToList();
+            List<vmClient> clientItem = new List<vmClient>();
+            foreach (var item in listOfClient)
+            {
+                clientItem.Add(new vmClient()
+                {
+                    Name = item.Name,
+                    Email = item.Email,
+                    Address = item.Address,
+                    CompanyName = item.CompanyName,
+                    Country = item.Country,
+                    Phone = item.Phone,
+                    Id = item.Id,
+                });
+            }
+
+            return clientItem;
+
+        }
+
+        public List<vmWorkTask> GetWorkData()
+        {
+            List<WorkTask> taskList = _context.ManagerTasks.Include(m => m.Manager).Include(s => s.SalesMan).ToList();
+            List<vmWorkTask> taskItem = new List<vmWorkTask>();
+
+            foreach (var item in taskList)
+            {
+                taskItem.Add(new vmWorkTask()
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    ToDate = item.ToDate,
+                    EndDate = item.EndDate,
+                    ChangeEndDate = item.EndDate.ToString("dddd dd MMMMM yyyy"),
+                    ChangeToDate = item.ToDate.ToString("dddd dd MMMMM yyyy "),
+                    Mark = item.Mark,
+                    Comment = item.Comment,
+                    Reply = item.Reply,
+                    Requirement = item.Requirement,
+                    CompliteTime = item.CompliteTime,
+                    ManagerName = item.Manager.Name,
+                    SalesManName = item.SalesMan.Name,
+                    ManagerId = item.ManagerId,
+                    SalesManId = item.SalesManId,
+                    PaidAmmount = item.PaidAmmount,
+                    DewAmmount = item.DewAmmount,
+                    IconClass = item.IconClass,
+                    IconColor = item.IconColor,
+                });
+            }
+            return taskItem;
+
+            
+        }
+
+        public List<WorkTask> GetList()
+        {
+            // List<WorkTask> list = new List<WorkTask>();
+
+            List<WorkTask> list =_context.ManagerTasks.ToList().ToList();
+
+            return list;
+        }
+
+     
+       
+    }
+}
---
+++ GetDropdownList.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using ProjectMaking.Data;
+using ProjectMaking.Models.EntityModel;
+using ProjectMaking.Models.ViewModel;
+using ProjectMaking.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectMaking.Helper
+{
+    public class GetDropdownList
+    {
+       internal List<SelectListItem> GetCountry()
+        {
+            List<SelectListItem> CountryList = new List<SelectListItem>();
+            CountryList.Add(new SelectListItem() { Text = "Bangladesh", Value = "Bangladesh" });
+            CountryList.Add(new SelectListItem() { Text = "India", Value = "India" });
+            CountryList.Add(new SelectListItem() { Text = "USA", Value = "USA" });
+            return CountryList;
+        }
+       internal List<SelectListItem> GetIconColor()
+        {
+            List<SelectListItem> IconColorList = new List<SelectListItem>();
+            IconColorList.Add(new SelectListItem() { Text = "icon-warning", Value = "icon-warning" });
+            IconColorList.Add(new SelectListItem() { Text = "icon-success", Value = "icon-success" });
+            IconColorList.Add(new SelectListItem() { Text = "icon-pink", Value = "icon-pink" });
+            IconColorList.Add(new SelectListItem() { Text = "icon-primary", Value = "icon-primary" });
+            return IconColorList;
+        }
+       internal List<SelectListItem> GetIconClass()
+        {
+
+            List<SelectListItem> IconClassList = new List<SelectListItem>();
+            IconClassList.Add(new SelectListItem() { Text = "mdi mdi-checkbox-marked-circle-outline", Value = "mdi mdi-checkbox-marked-circle-outline" });
+            IconClassList.Add(new SelectListItem() { Text = "mdi mdi-timer-off", Value = "mdi mdi-timer-off" });
+            IconClassList.Add(new SelectListItem() { Text = "mdi mdi-alert-decagram", Value = "mdi mdi-alert-decagram" });
+            IconClassList.Add(new SelectListItem() { Text = "mdi mdi-clipboard-alert", Value = "mdi mdi-clipboard-alert" });
+            IconClassList.Add(new SelectListItem() { Text = "mdi mdi-alert-decagram", Value = "mdi mdi-alert-decagram" });
+            return IconClassList;
+        }
+        internal List<SelectListItem> GetGender()
+        {
+            List<SelectListItem> dropdownList = new List<SelectListItem>();
+            dropdownList.Add(new SelectListItem() { Text = "Male", Value = "Male" });
+            dropdownList.Add(new SelectListItem() { Text = "Female", Value = "Female" });
+            dropdownList.Add(new SelectListItem() { Text = "Others", Value = "Others" });
+            return dropdownList;
+        }
+        internal List<SelectListItem> GetMaritailStatus()
+        {
+            List<SelectListItem> getmarried = new List<SelectListItem>();
+            getmarried.Add(new SelectListItem() { Text = "Married", Value = "Married" });
+            getmarried.Add(new SelectListItem() { Text = "UnMarried", Value = "UnMarried" });
+            return getmarried;
+        }
+        public List<SelectListItem> GetTotalDay()
+        {
+            List<SelectListItem> getDay = new List<SelectListItem>();
+            getDay.Add(new SelectListItem() { Text = "SatDay", Value = "SatDay" });
+            getDay.Add(new SelectListItem() { Text = "SunDay", Value = "SunDay" });
+            getDay.Add(new SelectListItem() { Text = "MonDay", Value = "MonDay" });
+            getDay.Add(new SelectListItem() { Text = "TuesDay", Value = "TuesDay" });
+            getDay.Add(new SelectListItem() { Text = "WednesDay", Value = "WednesDay"});
+            getDay.Add(new SelectListItem() { Text = "ThusDay", Value = "ThusDay" });
+            getDay.Add(new SelectListItem() { Text = "FriDay", Value = "FriDay" });
+            return getDay;
+        }
+    }
+}
